9d4d5d9565db7b93a3a972632f73ee9a9813f4ba
Jashandeep Singh <jashansingh303@gmail.com>
2022-12-11 16:27:29 -0500

    Initial layout of website

---
+++ pages/Contactpage.js
@@ -0,0 +1,13 @@
+import ContactUs from './components/contact'
+import _default from '@emailjs/browser'
+
+
+
+function Contactpage() {
+return(
+<></>
+)
+}
+
+export default Contactpage
+
---
+++ pages/components/contact.js
@@ -0,0 +1,33 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
+
+const ContactUs = () => {
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_sohnb1g', 'template_02kdq6h', form.current, 'ETh4mRn5hhRetAbvz')
+      .then((result) => {
+          console.log(result.text);
+          console.log("successful")
+      }, (error) => {
+          console.log(error.text);
+      });
+  };
+
+  return (
+    <form ref={form} onSubmit={sendEmail} className="mt-[200px] mb-[100px] md:max-w-xs px-[25px] bg-white text-black rounded-lg lg:my-[200px] flex flex-col mx-auto leading-10 max-w-[1000px]">
+      <h1 className='text-3xl text-center pt-10'>Contact Me</h1>
+      <label>Name</label>
+      <input type="text" name="user_name" className='text-black justify-center rounded-lg pl-3 bg-gray-300'/>
+      <label>Email</label>
+      <input type="email" name="user_email" className='text-black rounded-lg pl-3 bg-gray-300' />
+      <label>Message</label>
+      <textarea name="message" className='text-black rounded-lg pb-10 mb-5 pl-3 bg-gray-300'/>
+      <input type="submit" value="Send" className='m-10 bg-orange-600 w-20 mx-auto rounded-lg text-lg h-10 mb-10 text-white'/>
+    </form>
+  );
+};
+
+export default ContactUs
