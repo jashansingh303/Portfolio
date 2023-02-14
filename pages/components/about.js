2a79542b3e36ac3aade389e22d944eee19bc9088
Jashandeep Singh <jashansingh303@gmail.com>
2023-02-14 15:30:57 -0500

    Update 2/14/23

--- pages/components/about.js
+++ pages/components/about.js
@@ -5,7 +5,7 @@ function About() {
         <m.div initial={{opacity:0, x:"-100%"}} whileInView={{opacity:1, x:"0"}} transition={{duration: 1, ease:"easeInOut"}} className="uppercase min-h-screen text-center bg-gray-800">
             <div className="">
                 <h1 className="lg:text-8xl md:text-7xl text-[37px] text-orange-500 pt-32 ">A bit about me</h1>
-                <p className="lg:text-5xl md:text-2xl text-lg pt-4 lg:pt-32">I am a front-end developer based in New York City. I graduated with a bachelors in computer Science from Queens College. I am an avid gamer who likes to 
+                <p className="lg:text-5xl md:text-2xl text-lg pt-4 lg:pt-32">I am a software developer based in New York City. I graduated with a bachelors in computer Science from Queens College. I am an avid gamer who likes to 
                     spend his time creating websites and playing competitive games such as Overwatch and League Of Legends
                 </p>
             </div>
